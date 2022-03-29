c0144f185572e5cfdd9b56ea9dbeb001d41de67e
FireflyG2022 <102176791+FireflyG2022@users.noreply.github.com>
2022-03-29 18:44:09 +0200

    Create mQueue.cpp
    v 1.0 
    Basic template of programme

---
+++ mQueue.cpp
@@ -0,0 +1,17 @@
+/*
+//class implementing queue in C++
+//the same functionality as STL (Standard Template Library) clas queue in C++
+//with usage example
+//version 1.0
+Consists
+a. Program Template
+Author: Olaf Świetlik: Olimpijskie Koło Informatyczne
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
