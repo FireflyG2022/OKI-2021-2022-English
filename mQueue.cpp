9ddb48cb42b37137b966c974d121ac9e07d3260e
FireflyG2022 <102176791+FireflyG2022@users.noreply.github.com>
2022-03-29 19:01:00 +0200

    Update mQueue.cpp
    Added class mQueue

--- mQueue.cpp
+++ mQueue.cpp
@@ -2,16 +2,22 @@
 //class implementing queue in C++
 //the same functionality as STL (Standard Template Library) clas queue in C++
 //with usage example
-//version 1.0
+//version 1.1
 Consists
 a. Program Template
+b. Added class mQueue
 Author: Olaf Świetlik: Olimpijskie Koło Informatyczne
 */
 #include <iostream>
 #include <vector>
 using namespace std;
 
+class mQueue {
+   vector<int>elements;
+   int first,last;
+};
+
 int main(){
-    
+    mQueue payments;
     return 0;
 }
