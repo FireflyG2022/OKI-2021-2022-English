f12d6888e00060d9c310abbec166c87898518bcb
FireflyG2022 <102176791+FireflyG2022@users.noreply.github.com>
2022-03-29 19:59:31 +0200

    v 1.5 added Dequeue() method
    Whole code finished

--- mQueue.cpp
+++ mQueue.cpp
@@ -2,13 +2,14 @@
 //class implementing queue in C++
 //the same functionality as STL (Standard Template Library ) class queue in C++ 
 //with usage example
-//Version 1.4
+//Version 1.5
 Consists
 a. Program template
 b. defintion of class mqueue 
 c. Constructor
 d. empty() method
 e. push() method
+f. dequeue() method
 Author: Olaf Świetlik / Olimpijskie Kolo Informatyczne
  */
  
@@ -26,15 +27,30 @@ public:
         return true; 
     return false; 
  }	
- void push (int value){
- 	if(empty() == true){
- 		elements.push_back(value);
- 		first = last = 0;
- 	}
- 	elements.push_back(value);
- 	++last;
+ void push(int value) {
+    if (  empty() == true ) {
+       elements.push_back(value);
+	   first = last = 0;
+	   return;
+    }
+    elements.push_back(value);
+    ++last;
+ }	
+ int dequeue() {
+   int value;
+   if ( empty() ) {
+      return value;
+   }
+   if ( first == last) {
+      value = elements[first];
+      first = last = -1;
+	   elements.clear();
+	  return value;
+   }
+   value = elements[first];
+   ++first;
+   return value;
  }
- 
 private:
  vector<int> elements;
  int first, last;
@@ -42,10 +58,18 @@ private:
 
 int main() {
  mqueue	payments;
+ int value;
  
- cout << "a1: empty?:" << payments.empty() << "\n";
+ cout << "empty?: " << payments.empty() << "\n";
  payments.push(5);
- cout << "a2: empty?:" << payments.empty() << "\n";
-
+ payments.push(-3);
+ payments.push(8);
+ cout << "empty?: " << payments.empty() << "\n";
+ cout << "serviced: " << payments.dequeue() << "\n";
+ cout << "serviced: " << payments.dequeue() << "\n";
+ payments.push(11);
+ cout << "serviced: " << payments.dequeue() << "\n";
+ cout << "serviced: " << payments.dequeue() << "\n";
+ cout << "empty?: " << payments.empty() << "\n";
  return 0;
 }
